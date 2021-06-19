b6f080824a692b6557d182999cc10c6a86dc3594
Tauhidul Islam <abdurshahin5@gmail.com>
2021-06-19 21:46:55 +0600

    get all item found and single item request find

---
+++ server.js
@@ -0,0 +1,28 @@
+const fastify = require('fastify')({ logger: true });
+const PORT = 5000;
+
+const items = require('./Items')
+
+fastify.get('/items', (req, reply) => {
+  reply.send(items)
+})
+
+fastify.get('/items/:id', (req, reply) => {
+  const { id } = req.params
+
+  const item = items.find(item => item.id === id);
+
+  reply.send(item)
+})
+
+const start = async () => {
+  try {
+    await fastify.listen(PORT)
+  } catch (error) {
+    fastify.log.error(error)
+    process.exit(1)
+
+  }
+}
+
+start()
